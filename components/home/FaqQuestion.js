b4466adec6aa1d12aafd49d8ccaee2b5afb917e7
Smajl Mekaj <73967103+smajlmekaj@users.noreply.github.com>
2022-08-15 14:52:45 +0200

    Fixed styling

--- components/home/FaqQuestion.js
+++ components/home/FaqQuestion.js
@@ -5,30 +5,55 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import styled from '@emotion/styled';
+
+const AccordionWrapper = styled.div`
+  .css-1tom6df-MuiPaper-root-MuiAccordion-root:before {
+    height: 0;
+  }
+
+  .css-13859ar-MuiPaper-root-MuiAccordion-root:last-of-type {
+    border-radius: 0;
+  }
+  .css-13859ar-MuiPaper-root-MuiAccordion-root.Mui-expanded:last-of-type {
+    padding-bottom: 10px;
+  }
+`;
 
 const FaqQuestions = ({ title, description }) => {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
-    <Accordion sx={{ fontFamily: 'Inter' }}>
-      <AccordionSummary
-        onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
-        expandIcon={
-          isOpen ? (
-            <AddCircleOutlineIcon sx={{ color: '#22B56B' }} />
-          ) : (
-            <RemoveCircleOutlineIcon sx={{ color: '#22B56B' }} />
-          )
-        }
-        aria-controls="panel1a-content"
-        id="panel1a-header"
+    <AccordionWrapper>
+      <Accordion
+        sx={{
+          fontFamily: 'Inter',
+          borderRadius: 'none',
+          boxShadow: 'none',
+          borderBottom: '1px solid rgba(0, 0 , 0, 0.2)',
+        }}
       >
-        <Typography component="h2">{title}</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Typography>{description}</Typography>
-      </AccordionDetails>
-    </Accordion>
+        <AccordionSummary
+          onClick={() =>
+            isOpen ? setIsOpen(false) : setIsOpen(true)
+          }
+          expandIcon={
+            isOpen ? (
+              <AddCircleOutlineIcon sx={{ color: '#22B56B' }} />
+            ) : (
+              <RemoveCircleOutlineIcon sx={{ color: '#22B56B' }} />
+            )
+          }
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography component="h2">{title}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{description}</Typography>
+        </AccordionDetails>
+      </Accordion>
+    </AccordionWrapper>
   );
 };
 
